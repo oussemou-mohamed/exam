d6daab539871bb3d57475b84efe8ea124a56067f
git config --global user.name oussemou-mohamed <oussemou.med@gmail.com>
2024-01-01 00:47:17 +0100

    first commit

---
+++ resource-Service/src/main/java/com/oussemou/resourceService/sec/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.oussemou.resourceService.sec;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+                .oauth2ResourceServer(o2->o2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+
+}
